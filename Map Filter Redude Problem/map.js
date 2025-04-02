b5f15e6021dbc6a1300b11228d1151200bf8a756
Sewar77 <sewarjihad793@gmail.com>
2025-04-02 23:23:18 +0300

    commit some practicing exercises

--- Map Filter Redude Problem/map.js
+++ Map Filter Redude Problem/map.js
@@ -17,3 +17,41 @@ function doubleNumbers(array) {
   return array.map((element) => element * 2);
 }
 console.log(doubleNumbers([1, 2, 3, 4])); // [2, 4, 6, 8]
+
+/*
+Write a function capitalizeWords that takes an array of lowercase words and returns a new array where each word is capitalized.
+Example Input & Output:
+
+console.log(capitalizeWords(["hello", "world"]));
+*/
+
+capitalizeWords = function (array) {
+  return array.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+};
+
+console.log(capitalizeWords(["hello", "world"]));
+console.log("--------------------------------------------");
+
+let myarray = ["hello", "world"];
+const capital = myarray.map(
+  (word) => word.charAt(0).toUpperCase() + word.slice(1)
+);
+console.log(capital);
+console.log("--------------------------------------------");
+
+let products = [
+  { name: "laptop", price: 120, color: "white" },
+  { name: "phone", price: 130, color: "white" },
+  { name: "computer", price: 210, color: "white" },
+  { name: "laptop computer", price: 650, color: "white" },
+  { name: "computer laptop", price: 760, color: "white" },
+  { name: "phone laptop", price: 987, color: "white" },
+];
+
+const discount = products.map((products) => {
+  return {
+    ...products,
+    price: products.price * 0.5,
+  };
+});
+console.log(discount);
--- Map Filter Redude Problem/reduce.js
+++ Map Filter Redude Problem/reduce.js
@@ -19,3 +19,23 @@ console.log(sumNumbers([1, 2, 3, 4])); // 10
 var myarray = [1, 2, 3, 4];
 const summation  = myarray.reduce((a,b) => a+b);
 console.log(summation)
+
+
+/*
+Count Characters (reduce)
+Problem:
+Write a function countCharacters that takes a string
+and returns an object where keys are characters and values are the number of times they appear.
+Example Input & Output:
+
+console.log(countCharacters("hello")); 
+// { h: 1, e: 1, l: 2, o: 1 }
+*/
+
+let obj = {};
+countCharacters = function(word){
+    return word.reduce((a,b) => )
+}
+
+console.log(countCharacters("hello")); 
+
